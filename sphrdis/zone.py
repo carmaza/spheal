2bf1f1b96f3778f9c22de8cb6cba2dd754ea4f3b
Cristóbal Armaza <ca455@cornell.edu>
2022-11-12 16:15:39 -0500

    Change extents type to tuples

--- sphrdis/zone.py
+++ sphrdis/zone.py
@@ -9,21 +9,20 @@ class Zone:
     The surface of a spherical segment (excluding the bases).
     """
 
-    def __init__(self, extents: list, n_patches: int):
+    def __init__(self, extents: tuple, n_patches: int):
         self._extents = extents
-        self._patch_extents = [
-            2.0 * np.pi * m / n_patches for m in range(n_patches + 1)
-        ]
+        self._patch_extents = tuple(2.0 * np.pi * m / n_patches
+                                    for m in range(n_patches + 1))
 
     @property
-    def extents(self) -> list:
+    def extents(self) -> tuple:
         """
         The zenithal coordinates defining the zone.
         """
         return self._extents
 
     @property
-    def patch_extents(self) -> list:
+    def patch_extents(self) -> tuple:
         """
         The azimuthal extents of the patches that constitute the zone. The zone
         is divided into equal-shape patches.
--- tests/test_zone.py
+++ tests/test_zone.py
@@ -20,18 +20,18 @@ class TestZone(unittest.TestCase):
         np.random.seed(seed)
 
         n_patches = np.random.randint(2, 10)
-        extents = sorted(np.pi * np.random.rand(2))
+        extents = tuple(sorted(np.pi * np.random.rand(2)))
 
         zone = Zone(extents, n_patches)
         self.assertEqual(zone.extents,
                          extents,
                          msg="\n\nwhen testing {f}.\nRNG seed: {seed}.".format(
                              f="extents", seed=seed))
-        self.assertEqual(
-            [2.0 * np.pi * m / n_patches for m in range(n_patches + 1)],
-            zone.patch_extents,
-            msg="\n\nwhen testing {f}.\nRNG seed: {seed}.".format(
-                f="patch extents", seed=seed))
+        self.assertEqual(tuple(2.0 * np.pi * m / n_patches
+                               for m in range(n_patches + 1)),
+                         zone.patch_extents,
+                         msg="\n\nwhen testing {f}.\nRNG seed: {seed}.".format(
+                             f="patch extents", seed=seed))
 
 
 if __name__ == "__main__":
