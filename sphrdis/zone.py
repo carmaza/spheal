0a0357e6ddb01ffa4f7ef7cc63c25d7925465689
Cristóbal Armaza <ca455@cornell.edu>
2022-11-13 09:00:35 -0500

    Finish Zone dox

--- sphrdis/zone.py
+++ sphrdis/zone.py
@@ -7,6 +7,32 @@ import numpy as np
 class Zone:
     """
     The surface of a spherical segment (excluding the bases).
+
+    This class represents a natural unit for the tessellation of a hemisphere
+    consisting of cells whose boundaries are parallel and meridian arcs.
+
+    Parameters
+    ----------
+
+    `extents`: tuple
+    a 2-d tuple containing the zenithal extents of the zone.
+
+    `n_patches`: int
+    The number of azimuthal patches into which to divide the zone.
+
+    Members
+    -------
+
+    `extents`: tuple
+    The zenithal extents of the zone.
+
+    `patch_extents`: tuple
+    The azimuthal extents of the patches that constitute the zone.
+
+    Notes
+    -----
+
+    - Each patch has exactly the same shape.
     """
 
     def __init__(self, extents: tuple, n_patches: int):
@@ -16,15 +42,8 @@ class Zone:
 
     @property
     def extents(self) -> tuple:
-        """
-        The zenithal coordinates defining the zone.
-        """
         return self._extents
 
     @property
     def patch_extents(self) -> tuple:
-        """
-        The azimuthal extents of the patches that constitute the zone. The zone
-        is divided into equal-shape patches.
-        """
         return self._patch_extents
