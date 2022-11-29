ee2e313782be3b4d452fb84d5bf0ef6914b2cc87
Cristóbal Armaza <ca455@cornell.edu>
2022-11-28 21:27:50 -0500

    Add Hemisphere tesselation

---
+++ spheal/hemisphere.py
@@ -0,0 +1,147 @@
+# Distributed under the MIT License.
+# See LICENSE for details.
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+from spheal.zone import Zone
+
+
+class Hemisphere:
+    """
+    Equal-area disk tessellation based on Beckers & Beckers (2012).
+
+    This tesselation accommodates an exact given number of patches on the
+    hemisphere. The hemisphere is divided into zones of variable number of
+    patches (i.e. no congruency), and every patch throughout the hemisphere
+    has exactly the same surface area. 
+
+    Parameters
+    ----------
+
+    `radius`: float
+    The radius of the hemisphere.
+
+    `n_patches`: int
+    The total number of patches to use in the tessellation.
+
+    `aspect`: float
+    The constant aspect ratio that the algorithm tries to give to each patch.
+
+    `draw`: bool (default: False)
+    Whether to draw the resulting tessellation to a PDF.
+
+    Members
+    -------
+    
+    `zones`: list
+    The zones that constitute the hemisphere.
+
+    `radius` : float
+    The radius of the hemisphere.
+
+    `patch_number`: int
+    The total number of patches in the hemisphere.
+
+    Notes
+    -----
+
+    - Every patch's aspect ratio will only be _approximately_ equal to the given
+      aspect ratio. This is because the aspect ratio is computed given the
+      number of patches at two adjacent zones, and float rounding happens
+      when computing the number of patches in each zone.
+
+    - For a given number of patches, aspect ratios too low or too high might
+      fail to converge to a solution. For instance, for 1000 patches, aspect
+      ratios > 50 and < 0.15 are prone to give inaccurate results.
+
+    """
+
+    def __init__(self,
+                 radius: float,
+                 n_patches: int,
+                 patch_aspect: float,
+                 draw=False):
+        self._radius = radius
+        self._patch_aspect = patch_aspect
+
+        l = 0
+        theta_lm1 = 0.5 * np.pi
+        r_lm1 = self._r(theta_lm1)
+        k_lm1 = n_patches
+
+        # Maximum integer l for which theta_l > 0.
+        lmax = np.floor(
+            (radius * theta_lm1 / r_lm1) *
+            np.sqrt(n_patches * patch_aspect / np.pi)).astype(dtype=np.int64)
+
+        self._zones = []
+        while (l < lmax):
+            l += 1
+            theta_l = self._theta_l(theta_lm1, r_lm1, k_lm1)
+            r_l = self._r(theta_l)
+            k_l = self._k_l(k_lm1, r_lm1, r_l)
+
+            if l == lmax:
+                r_inn = r_lm1 / np.sqrt(k_lm1)
+                theta_inn = 2. * np.arcsin(0.5 * r_inn / radius)
+                self._zones.append(Zone((theta_inn, theta_lm1), k_lm1 - 1))
+                self._zones.append(Zone((0., theta_inn), 1))
+
+            else:
+                self._zones.append(Zone((theta_l, theta_lm1), k_lm1 - k_l))
+                theta_lm1, r_lm1, k_lm1 = theta_l, r_l, k_l
+
+        if draw:
+            self.draw_lambert_proj()
+
+    @property
+    def zones(self):
+        return self._zones
+
+    @property
+    def radius(self):
+        return self._radius
+
+    @property
+    def patch_number(self):
+        return sum(zone.patch_number for zone in self._zones)
+
+    # Eq. (1)
+    def _k_l(self, k_lm1, r_lm1, r_l):
+        return np.rint(k_lm1 * (r_l / r_lm1)**2.0).astype(dtype=np.int64)
+
+    # Eq. (16)
+    def _r(self, theta):
+        return 2.0 * self._radius * np.sin(0.5 * theta)
+
+    # Eq. (20)
+    def _theta_l(self, theta_lm1, r_lm1, k_lm1):
+        return theta_lm1 - r_lm1 * np.sqrt(
+            np.pi / self._patch_aspect / k_lm1) / self._radius
+
+    def draw_lambert_proj(self, name="Projection"):
+        """
+        Draw Lambert projection of the tesselation to PDF.
+        """
+        dense_phi = np.linspace(0., 2. * np.pi, 100)
+        dense_cos, dense_sin = np.cos(dense_phi), np.sin(dense_phi)
+
+        fig, ax = plt.subplots()
+        for zone in self._zones:
+            ri, ro = self._r(zone.extents[0]), self._r(zone.extents[1])
+            phi = zone.patch_extents
+            cos_phi, sin_phi = np.cos(phi), np.sin(phi)
+            if len(phi) > 2:
+                ax.plot(np.array([ri * cos_phi, ro * cos_phi]),
+                        np.array([ri * sin_phi, ro * sin_phi]),
+                        color="purple",
+                        linewidth=0.3)
+            ax.plot(ro * dense_cos,
+                    ro * dense_sin,
+                    color="purple",
+                    linewidth=0.3)
+
+        ax.set_aspect(1.0)
+        plt.savefig("{}.pdf".format(name), bbox_inches="tight")
+        plt.close()
---
+++ tests/test_hemisphere.py
@@ -0,0 +1,85 @@
+# Distributed under the MIT License.
+# See LICENSE for details.
+
+from context import spheal
+
+import numpy as np
+import unittest
+
+from spheal.hemisphere import Hemisphere
+
+
+class TestHemisphere(unittest.TestCase):
+    """
+    Test `Hemisphere` class.
+    """
+
+    def test(self):
+
+        seed = np.random.randint(0, 1e6)
+        np.random.seed(seed)
+
+        # Test properties of random tessellation.
+        radius = np.random.rand()
+        n_patches = np.random.randint(10, 100)
+
+        # Choose random healthy patch aspect ratio.
+        patch_aspect = np.random.rand()
+        while (patch_aspect > 4. or patch_aspect < 0.5):
+            patch_aspect = np.random.rand()
+
+        hemisphere = Hemisphere(radius, n_patches, patch_aspect)
+
+        # Check that exactly `n_patches` patches have been created.
+        self.assertEqual(hemisphere.patch_number,
+                         n_patches,
+                         msg="\n\nwhen testing {f}.\nRNG seed: {seed}.".format(
+                             f="patch number", seed=seed))
+
+        # TO-DO: area test not passing. Likely related to not being able to
+        # reproduce Table 6, line 5 (zenithal angles).
+
+        # # Check that the surface area of all inner polar caps per patch equals
+        # # area of the outer ones.
+        # area = 2. * np.pi * radius**2. * (1. - np.cos(0.5 * np.pi)) / n_patches
+        # number_sub = n_patches
+        # print(area)
+        # for zone in hemisphere.zones:
+        #     theta_sub = zone.extents[1]
+        #     area_sub = 2. * np.pi * radius**2. * (1. - np.cos(theta_sub)) / number_sub
+        #     number_sub -= zone.patch_number
+        #     self.assertAlmostEqual(
+        #         area,
+        #         area_sub,
+        #         msg="\n\nwhen testing {f}.\nRNG seed: {seed}.".format(
+        #             f="area within theta = {}".format(str(theta_sub)), seed=seed))
+
+        def get_partial_numbers(hemisphere):
+            patch_numbers = []
+            partial_number = 0
+            for zone in sorted(hemisphere.zones):
+                partial_number += zone.patch_number
+                patch_numbers.append(partial_number)
+            return patch_numbers
+
+        # Get with Beckers & Beckers 2012, Table 5: Hemisphere.
+        hemisphere_table_5 = Hemisphere(radius=1.,
+                                        n_patches=288,
+                                        patch_aspect=1.)
+        self.assertEqual(get_partial_numbers(hemisphere_table_5),
+                         [1, 9, 22, 41, 65, 94, 128, 165, 205, 246, 288],
+                         msg="\n\nwhen testing {f}.\nRNG seed: {seed}.".format(
+                             f="comparison with BB12 Table 5", seed=seed))
+
+        # Get Beckers & Beckers 2012, Table 6, line 2.
+        hemisphere_table_6 = Hemisphere(radius=1.,
+                                        n_patches=145,
+                                        patch_aspect=1.)
+        self.assertEqual(get_partial_numbers(hemisphere_table_6),
+                         [1, 8, 20, 38, 60, 86, 115, 145],
+                         msg="\n\nwhen testing {f}.\nRNG seed: {seed}.".format(
+                             f="comparison with BB12 Table 6", seed=seed))
+
+
+if __name__ == "__main__":
+    unittest.main()
