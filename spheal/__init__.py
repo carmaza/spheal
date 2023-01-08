ed91e0cebe13aa71dc77a712cab67369e94dfa2a
Cristóbal Armaza <ca455@cornell.edu>
2023-01-08 12:37:49 -0500

    Expose Annulus

--- spheal/__init__.py
+++ spheal/__init__.py
@@ -1,6 +1,7 @@
 # Distributed under the MIT License.
 # See LICENSE for details.
 
+from .annulus import Annulus
 from .disk import Disk
 from .euclidean import *
 from .hemisphere import Hemisphere
