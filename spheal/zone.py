5c02c1cbe4b8e59d224b45c21d3ccc2107608cd8
Cristóbal Armaza <ca455@cornell.edu>
2023-01-19 17:13:53 -0500

    Move and add missing docstrings

--- spheal/zone.py
+++ spheal/zone.py
@@ -1,5 +1,9 @@
 # Distributed under the MIT License.
 # See LICENSE for details.
+"""
+Defines class `Zone`.
+
+"""
 
 import numpy as np
 
@@ -10,15 +14,6 @@ class Zone:
 
     This class represents a natural unit for the tessellation of a hemisphere
     consisting of cells whose boundaries are parallel and meridian arcs.
-
-    Parameters
-    ----------
-
-    `extents`: tuple
-    a 2-d tuple containing the zenithal extents of the zone.
-
-    `n_patches`: int
-    The number of azimuthal patches into which to divide the zone.
 
     Members
     -------
@@ -36,9 +31,21 @@ class Zone:
     -----
 
     - Each patch has exactly the same shape.
+
     """
 
     def __init__(self, extents: tuple, n_patches: int):
+        """
+        Parameters
+        ----------
+
+        `extents`: tuple
+        A 2-d tuple containing the zenithal extents of the zone.
+
+        `n_patches`: int
+        The number of azimuthal patches into which to divide the zone.
+
+        """
         self._extents = extents
         self._patch_extents = tuple(2.0 * np.pi * m / n_patches
                                     for m in range(n_patches + 1))
@@ -48,12 +55,24 @@ class Zone:
 
     @property
     def extents(self) -> tuple:
+        """
+        The radial extents of the zone.
+
+        """
         return self._extents
 
     @property
     def patch_number(self) -> int:
+        """
+        The number of patches covering the zone.
+
+        """
         return len(self._patch_extents) - 1
 
     @property
     def patch_extents(self) -> tuple:
+        """
+        The angular extents of the patches.
+
+        """
         return self._patch_extents
