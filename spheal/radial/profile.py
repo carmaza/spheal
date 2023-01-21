03c09e3546f6ad4ac3b6303e6ac264c54b7dad60
Cristóbal Armaza <ca455@cornell.edu>
2023-01-20 20:56:36 -0500

    Document class

--- spheal/radial/profile.py
+++ spheal/radial/profile.py
@@ -1,17 +1,44 @@
 # Distributed under the MIT License.
 # See LICENSE for details.
+"""
+Defines `Profile`, the base class for radial profiles.
+
+"""
 
 import abc
 
 
 class Profile(metaclass=abc.ABCMeta):
+    """
+    Base class for radial profiles. Each derived class should define the
+    following members:
+
+    Attributes
+    ----------
+
+    `r90` : float
+    The radius containing 90% of the total number of particles.
+
+    Functions
+    ---------
+
+    `particle_number(r)`
+    The number of particles contained at the given radius r.
+
+    """
 
     @property
     @abc.abstractmethod
     def r90(self):
-        pass
+        """
+        Return the radius enclosing 90% of the particles.
+
+        """
 
     @staticmethod
     @abc.abstractmethod
     def particle_number(r):
-        pass
+        """
+        Compute the number of particles enclosed in a radius r.
+
+        """
