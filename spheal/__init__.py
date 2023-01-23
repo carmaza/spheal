8930c60f59d7f393196f530cfa6dc049280fb60a
Cristóbal Armaza <ca455@cornell.edu>
2023-01-22 23:33:33 -0500

    Add package docstring

--- spheal/__init__.py
+++ spheal/__init__.py
@@ -1,5 +1,17 @@
 # Distributed under the MIT License.
 # See LICENSE for details.
+"""
+Imports the classes related to spherical elements and algorithms.
+
+- `Annulus`
+- `Disk`
+- `Hemisphere`
+- `Zone`
+
+It also imports functions related to vector algebra in Euclidean geometry via
+the module `euclidean`.
+
+"""
 
 from .annulus import Annulus
 from .disk import Disk
